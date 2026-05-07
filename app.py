ab88595f633a9e4c6db50d8301bf58b1b3146823
LazarteThiago01 <lazartethiago2501@gmail.com>
2026-05-06 22:40:26 -0300

    Thiago: Agregue la base de datos y los turnos

--- app.py
+++ app.py
@@ -23,3 +23,54 @@ def reservar():
 if __name__ == '__main__':
     # El modo debug=True permite que el servidor se reinicie solo cada vez que guardas un cambio
     app.run(debug=True)
+
+#base de datos
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy # Nueva librería
+
+app = Flask(__name__)
+
+# Configuración de la Base de Datos
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///barberia.db'
+db = SQLAlchemy(app)
+
+# Definimos cómo se guarda un Turno
+class Turno(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    peluquero = db.Column(db.String(50), nullable=False)
+    fecha = db.Column(db.String(50), nullable=False)
+
+# Creamos el archivo de la base de datos
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/reservar', methods=['POST'])
+def reservar():
+    # 1. Obtenemos los datos del formulario
+    nombre = request.form.get('nombre')
+    email = request.form.get('email')
+    peluquero = request.form.get('peluquero')
+    fecha = request.form.get('fecha')
+
+    # 2. Creamos el objeto Turno y lo guardamos en la DB
+    nuevo_turno = Turno(nombre=nombre, email=email, peluquero=peluquero, fecha=fecha)
+    db.session.add(nuevo_turno)
+    db.session.commit()
+
+    return f"Turno guardado en la base de datos para {nombre}!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# vista para peluqueros
+@app.route('/turnos')
+def ver_turnos():
+    # Consultamos todos los turnos guardados en la base de datos
+    todos_los_turnos = Turno.query.all()
+    return render_template('turnos.html', lista_turnos=todos_los_turnos)
